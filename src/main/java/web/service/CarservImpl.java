b83aae05ee7b3f900838bbf907785f57ac513053
NGUfromPB <99163430+NGUfromPB@users.noreply.github.com>
2023-03-10 17:29:13 +0300

    Update CarservImpl.java

--- src/main/java/web/service/CarservImpl.java
+++ src/main/java/web/service/CarservImpl.java
@@ -1,15 +1,22 @@
-package web.service;
 
+import org.springframework.stereotype.Service;
 import web.models.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class CarservImpl implements Carserv {
-
     @Override
-    public List<Car> ans(List<Car> ans,Integer c) {
-        return ans.stream().limit(c).collect(Collectors.toList());
+    public List<Car> ans(Integer c) {
+        List<Car> carList; {
+            carList=new ArrayList<>();
+            carList.add(new Car("mazda", (byte) 1,1));
+            carList.add(new Car("honda", (byte) 2,2));
+            carList.add(new Car("lada", (byte) 3,3));
+            carList.add(new Car("mers", (byte) 4,4));
+            carList.add(new Car("spogetti", (byte) 5,5));
+        }
+        return carList.stream().limit(c).collect(Collectors.toList());
     }
 }
