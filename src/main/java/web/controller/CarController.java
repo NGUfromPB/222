ae765e35eb0c79eb5a581c74a7e1cec4aa495f28
NGUfromPB <99163430+NGUfromPB@users.noreply.github.com>
2023-03-10 17:28:04 +0300

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,30 +1,22 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
 import web.service.Carserv;
-import web.service.CarservImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
+    @Autowired
+    private Carserv carserv;
     @GetMapping(value ="/cars")
     public String print(@RequestParam(defaultValue = "5") Integer c, ModelMap  model) {
-        Carserv carserv=new CarservImpl();
-        List<Car> carList; {
-            carList=new ArrayList<>();
-            carList.add(new Car("mazda", (byte) 1,1));
-            carList.add(new Car("honda", (byte) 2,2));
-            carList.add(new Car("lada", (byte) 3,3));
-            carList.add(new Car("mers", (byte) 4,4));
-            carList.add(new Car("spogetti", (byte) 5,5));
-        }
-        List<Car> ans = carserv.ans(carList,c);
+        List<Car> ans = carserv.ans(c);
         model.addAttribute("cars",ans);
         return "cars";
     }
